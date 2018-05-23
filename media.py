46d692826a1c96fa6a39b7427609bf487d8fb9b5
15pa1a0497 <39545553+15pa1a0497@users.noreply.github.com>
2018-05-23 17:00:25 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,5 +1,12 @@
+import webbrowser
+""" This class provides a way to store movie related information
+    Attributes:
+        title: The title of the movie
+        storyline: A short description of what the movie is about
+        poster_image_url: The URL address of a poster of the movie
+        trailer_youtube_url: A link to a YouTube.com"""
 class movie():
-    # Creatig class movie
+    
     def __init__(self, movie_title, movie_storyline,
                  poster_image, trailer_youtube):
         # Defining class constructor
@@ -7,4 +14,9 @@ class movie():
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+    
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+
+
         
